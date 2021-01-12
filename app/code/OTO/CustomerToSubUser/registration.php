c1cc13c19b4ca08295e73fa5576d137d63632b23
Lục Thần Ca <lucthanca@gmail.com>
2021-01-13 00:54:34 +0700

    create customize module oto/customer-to-sub-user

---
+++ app/code/OTO/CustomerToSubUser/registration.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types = 1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'OTO_CustomerToSubUser',
+    __DIR__
+);
