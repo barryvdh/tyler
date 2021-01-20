8eac21291359d13bcf5acf9e334a867f3cbc2c88
Hue Le <huelt@bsscommerce.com>
2021-01-20 10:41:48 +0000

    create atribute category and style page category(X1B2BTyler-13)

---
+++ app/code/Bss/CategoryAttributes/Setup/InstallData.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * BSS Commerce Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://bsscommerce.com/Bss-Commerce-License.txt
+ *
+ * @category   BSS
+ * @package    Bss_CategoryAttributes
+ * @author     Extension Team
+ * @copyright  Copyright (c) 2017-2018 BSS Commerce Co. ( http://bsscommerce.com )
+ * @license    http://bsscommerce.com/Bss-Commerce-License.txt
+ */
+namespace Bss\CategoryAttributes\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Eav setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'short_description',
+            [
+                'type' => 'text',
+                'label' => 'Short Description',
+                'input' => 'textarea',
+                'required' => false,
+                'sort_order' => 4,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'wysiwyg_enabled' => true,
+                'is_html_allowed_on_front' => true,
+                'group' => 'General Information',
+            ]
+        );
+    }
+}
