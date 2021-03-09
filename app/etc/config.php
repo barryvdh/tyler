470046b9ff7fdba35c17951637faffcef69a8033
vadu <dunghv@bsscommerce.com>
2021-03-09 13:43:38 +0700

    customize changes of amasty role permissions (X1B2BTyler-50)

--- app/etc/config.php
+++ app/etc/config.php
@@ -299,6 +299,7 @@ return [
         'Bss_CompanyAccount' => 1,
         'Bss_CustomerAttributes' => 1,
         'Bss_CustomerToSubUser' => 1,
+        'Bss_CustomizeAmastyRolePermissions' => 1,
         'Bss_CustomizeForceLogin' => 1,
         'Bss_B2bRegistration' => 1,
         'Bss_HideProductField' => 1,
